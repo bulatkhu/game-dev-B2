47fef347623447d7b9437d459fad3305368c5d94
bulatkhu <hudainazarov.b@gmail.com>
2023-05-14 12:46:46 +0200

    Added flying animations

--- Assets/Scripts/NewPlayer.cs
+++ Assets/Scripts/NewPlayer.cs
@@ -2,13 +2,58 @@
 
 public class NewPlayer : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    
     private SpriteRenderer spriteRenderer;
     public Sprite[] sprites;
     private int spriteIndex;
 
-    private void Awake()
+    public float strength = 5f;
+    public float gravity = -9.81f;
+    public float tilt = 5f;
+    private bool isCollided;
+
+    void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void OnEnable()
+    {
+        Vector3 position = transform.position;
+        position.y = 0f;
+        transform.position = position;
+        rb.velocity = Vector2.zero;
+        isCollided = false;
+    }
+
+    private void Update()
+    {
+        if (isCollided)
+        {
+            return;
+        }
+
+        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * strength;
+        }
+
+        rb.velocity += Vector2.up * gravity * Time.deltaTime;
+
+        float angle = Mathf.Lerp(0f, -tilt, rb.velocity.y / 3f);
+        rb.rotation = angle;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"GO: {collision.gameObject.name}");
+        if (!isCollided)
+        {
+            isCollided = true;
+        }
     }
 
     private void Start()
@@ -28,5 +73,4 @@ public class NewPlayer : MonoBehaviour
             spriteRenderer.sprite = sprites[spriteIndex];
         }
     }
-
 }
