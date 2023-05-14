986dea31e551e43291a2300d9635897da3ad8f39
bulatkhu <hudainazarov.b@gmail.com>
2023-05-14 12:01:42 +0200

    initial implementation

---
+++ Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class NewPlayer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length) {
+            spriteIndex = 0;
+        }
+
+        if (spriteIndex < sprites.Length && spriteIndex >= 0) {
+            spriteRenderer.sprite = sprites[spriteIndex];
+        }
+    }
+
+}
