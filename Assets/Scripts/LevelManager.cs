beadb14611f4cee47058ece5c8a2d586410f6c2f
bulatkhu <hudainazarov.b@gmail.com>
2023-05-17 21:58:39 +0200

    Added initial level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;   
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void StartLevel1()
+    {
+        SceneManager.LoadScene("Scenes/Level 1");
+    }
+    
+    public void StartLevel2()
+    {
+        SceneManager.LoadScene("Scenes/Level 1");
+    }
+    
+    public void StartLevel3()
+    {
+        SceneManager.LoadScene("Scenes/Level 1");
+    }
+}
